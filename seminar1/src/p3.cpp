53d80f90de09b0b3718974fb52d5bfee2ad221af
Andrei Aldea <aldea.andrei.977@gmail.com>
2021-02-18 13:38:13 +0200

    finished problem 3

--- seminar1/src/p3.cpp
+++ seminar1/src/p3.cpp
@@ -21,11 +21,45 @@ int split(char *propozitie, const char* delim, char *cuvinte[128][32])
     return i;
 }
 
+// ? Simple bubble sort
+// TODO:: make this a better sorting algorithm and make it [[noexcept]]
+void sort(char *cuvinte[128][32], unsigned int wordc)
+{
+    bool done = false;
+    while(!done)
+    {
+        done = true;
+        for(unsigned int i = 0; i < wordc - 1; ++i)
+        {
+            if(strlen((*cuvinte)[i]) < strlen((*cuvinte)[i+1]))
+            {
+                char *tmp = (*cuvinte)[i];
+                (*cuvinte)[i] = (*cuvinte)[i+1]; 
+                (*cuvinte)[i+1] = tmp;
+                done = false;
+            }
+            else if (strlen((*cuvinte)[i]) == strlen((*cuvinte)[i+1]) && strcmp( (*cuvinte)[i],(*cuvinte)[i+1]) > 0)
+            {
+                char *tmp = (*cuvinte)[i];
+                (*cuvinte)[i] = (*cuvinte)[i+1]; 
+                (*cuvinte)[i+1] = tmp;
+                done = false;
+            }
+        }
+    }
+}
+
 
 int main(int argc, char** argv)
 {
     char propozitie[256];
     scanf_s("%[^\n]s", propozitie);
     char *cuvinte[32];
-    split(propozitie, " ", &cuvinte);
+    int word_c = split(propozitie, " ", &cuvinte);
+    sort(&cuvinte, word_c);
+
+    for(unsigned int i = 0 ; i < word_c; ++i)
+    {
+        puts(cuvinte[i]);
+    }
 }
