258dbef3b479a99108da19a3a5b29a0bb6c25c36
Andrei Aldea <aldea.andrei.977@gmail.com>
2021-02-18 09:49:31 +0200

    lat commit of the lab

--- seminar1/src/p3.cpp
+++ seminar1/src/p3.cpp
@@ -2,19 +2,30 @@
 #include <cstdlib>
 #include <cstring>
 
+
+/*
+ * @return the number of words split
+*/
+int split(char *propozitie, const char* delim, char *cuvinte[128][32])
+{
+    char* cuvant;
+    char *next_token;
+    cuvant = strtok_s(propozitie, delim, &next_token);
+    unsigned int i = 0; 
+    while(cuvant != NULL)
+    {
+        (*cuvinte)[i] = cuvant;
+        i++;
+        cuvant = strtok_s(NULL , delim, &next_token);
+    }
+    return i;
+}
+
+
 int main(int argc, char** argv)
 {
     char propozitie[256];
-    scanf("%[^\n]s", propozitie);
-    char cuvinte[128][32];
-    char *cuvant;
-    cuvant = strtok( propozitie, " ");
-    unsigned int i = 0;
-    while( cuvant != NULL)
-    {
-        cuvinte[i] = cuvant;
-        cuvant = strtok( propozitie, " ");
-    }
-
-    printf("%s",propozitie);
+    scanf_s("%[^\n]s", propozitie);
+    char *cuvinte[32];
+    split(propozitie, " ", &cuvinte);
 }
