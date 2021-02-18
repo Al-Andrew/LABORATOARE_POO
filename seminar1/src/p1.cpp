f1971edc044111239c267115bf2ba22ea97befc4
Andrei Aldea <aldea.andrei.977@gmail.com>
2021-02-18 09:09:41 +0200

    p1 si p4 be doen

---
+++ seminar1/src/p1.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+
+int scruffy_atoi(const char* input)
+{
+    unsigned int len = strlen(input);
+    int sol = 0;
+    for(unsigned int i = 0; i < len ; ++i )
+    {
+        if( input[i] >= '0' && input[i] <= '9')
+        {
+            sol = sol * 10 + (input[i] - '0');
+        }
+    }
+    return sol;
+}
+
+
+int main(int argc, char** argv)
+{
+    char nr[128];
+    FILE* fisier;
+    int suma = 0;
+    //!! THIS CAN BE NULL
+    fisier = fopen("ini.txt", "r");
+    while( fisier != NULL && !feof(fisier) )
+    {
+        fgets(nr, 127, fisier);
+        //TODO : add whitespace trimming
+        suma += scruffy_atoi(nr);
+    }
+    printf("%d", suma );
+}
