7bb8302baf2f739890ddcc633d95ece47ec2a862
Andrei Aldea <aldea.andrei.977@gmail.com>
2021-05-13 20:38:56 +0300

    First test now passses

--- seminar11/test/src/main.cpp
+++ seminar11/test/src/main.cpp
@@ -1,9 +1,16 @@
-#include <iostream>
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 
-using namespace std;
+#include "Sort.hpp"
+#include "SortFixed.hpp"
 
-int main(int argc, char** argv)
-{
-    cout << "hello world!" << endl;
-    return 0;
+#include "TestingHelpers.hpp"
+
+
+TEST_CASE( "INTS", "[sort]"){
+    SECTION("ALREADY SORTED"){
+        int a[11] = {1,2,3,4,5,6,7,8,9,10};
+        SortFixed<int>::QuickSort(a, 10);
+        CHECK( il<int>({1,2,3,4,5,6,7,8,9,10}) ==  a );
+    }
 }
