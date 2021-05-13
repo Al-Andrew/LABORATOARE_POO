ed99141d94712d5eec3e5a25dcba0e3f358cf904
Andrei Aldea <aldea.andrei.977@gmail.com>
2021-05-13 20:41:26 +0300

    Inverted test passes

--- seminar11/test/src/main.cpp
+++ seminar11/test/src/main.cpp
@@ -13,4 +13,9 @@ TEST_CASE( "INTS", "[sort]"){
         SortFixed<int>::QuickSort(a, 10);
         CHECK( il<int>({1,2,3,4,5,6,7,8,9,10}) ==  a );
     }
+    SECTION("INVERTED"){
+        int a[11] = {10,9,8,7,6,5,4,3,2,1};
+        SortFixed<int>::QuickSort(a, 10);
+        CHECK( il<int>({1,2,3,4,5,6,7,8,9,10}) ==  a );
+    }
 }
